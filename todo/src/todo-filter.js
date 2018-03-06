088153f6e71efdea5d0faf0adfe924b6c31e2cac
Saad Ulde <saad.ulde@claricetechnologies.com>
2018-03-06 23:38:30 +0530

    1. Integrating count in the Todo Filter component

--- todo/src/todo-filter.js
+++ todo/src/todo-filter.js
@@ -1,16 +1,14 @@
 import React, {Component} from 'react';
 
-class TodoFilter extends Component {
-    render() {
-        return (
-            <div>
-                <span>Count : </span>
-                <span>All</span>
-                <span>Active</span>
-                <span>Completed</span>
-            </div>
-        );
-    }
+const TodoFilter = (props) => {
+    return (
+        <div>
+            <span>Count : {props.todoList.length}</span>
+            <span>All</span>
+            <span>Active</span>
+            <span>Completed</span>
+        </div>
+    );
 }
 
 export default TodoFilter;
