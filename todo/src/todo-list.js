de1142226117c3a7ea3b36425c576924e30924c6
Saad Ulde <saad.ulde@claricetechnologies.com>
2018-03-06 23:31:46 +0530

    1. Converting Todo List to stateless 2. Added placeholder methods for adding and updating todos

--- todo/src/todo-list.js
+++ todo/src/todo-list.js
@@ -1,17 +1,18 @@
 import React, {Component} from 'react';
 
-class TodoList extends Component {
-    render() {
-        return (
-            <div>
-                <ul>
-                    <li>
-                        Test
-                    </li>
-                </ul>
-            </div>
-        );
-    }
+const TodoList = (props) => {
+    console.log(props);
+    return (
+        <div>
+            <ul>
+                {
+                    props.todoList.map(item => {
+                        return <li key={item.id}> {item.text} </li>;
+                    })
+                }
+            </ul>
+        </div>
+    );
 }
 
 export default TodoList;
--- todo/src/todo.js
+++ todo/src/todo.js
@@ -4,12 +4,50 @@ import TodoList from './todo-list';
 import TodoFilter from './todo-filter';
 
 class Todo extends Component {
+    constructor() {
+        super();
+        this.state = {
+            todoList: [{
+                text: "test123",
+                id: 1,
+                status: false
+            }]
+        }
+    }
+
+    addTodo(text) {
+
+        var todoItem = {
+            text: text,
+            id: this.state.todoList.length,
+            status: false
+        };
+
+        this.setState({
+            todoList: this.state.todoList.push(todoItem)
+        });
+    }
+
+    updateTodo(updatedTodoItem) {
+        var updatedTodoList = this.state.todoList.map(todoItem => {
+            if(todoItem.id === updatedTodoItem.id) {
+                todoItem.status = true;
+            }
+            return todoItem;
+        });
+        this.setState({
+            todoList: updatedTodoList
+        });
+    }
+
     render() {
         return (
             <div>
+                {/* <TodoCreate addTodo={this.addTodo(text).bind(this)} /> */}
                 <TodoCreate />
-                <TodoList /> 
-                <TodoFilter />
+                <TodoList todoList={this.state.todoList} />
+                {/* <TodoFilter todoList={this.state.todoList} updateTodo={this.updateTodo(todoItem).bind(this)} /> */}
+                <TodoFilter todoList={this.state.todoList} />
             </div>
         );
     }
