6015f0dfaf96da5dc71b2904d274139c8d19eaed
Saad Ulde <saad.ulde@claricetechnologies.com>
2018-03-18 17:22:28 +0530

    1. Adding basic styling and moment 2. Adding logic to conditionally show the result of the test

--- interview-scheduler/src/candidate-table.js
+++ interview-scheduler/src/candidate-table.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.css';
 import cellEditFactory from 'react-bootstrap-table2-editor';
 import MockData from './mock-data.json';
+import moment from 'moment';
 
 class CandidateTable extends Component {
 
@@ -16,14 +19,32 @@ class CandidateTable extends Component {
         }, {
             dataField: 'testDetails.score',
             text: 'Test Score'
+        }, {
+            dataField: 'testDetails.startTime',
+            text: 'Test Start',
+            editable: (content, row, rowIndex, columnIndex) => {
+                return row.testDetails.score > 3;
+            },
+            formatter: (cell, row) => {
+
+                if (row.testDetails.score < 3) {
+                    return "Rejected";
+                } else {
+                    return "Selected";
+                }
+            }
         }];
+
+        const cellEdit = cellEditFactory({
+            mode: 'dbclick'
+        });
 
         return (
             <BootstrapTable
                 keyField="emailId"
                 data={MockData.data}
                 columns={columns}
-                cellEdit={cellEditFactory({ mode: 'dbclick' })}
+                cellEdit={cellEdit}
             />
         );
     };
