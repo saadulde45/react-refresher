d9d1c16dfdc9835e3f35f0e156aaf1ced1fabe97
Saad Ulde <saad.ulde@claricetechnologies.com>
2018-03-20 01:27:02 +0530

    1. Adding back the colum formatters

--- interview-scheduler/src/App.js
+++ interview-scheduler/src/App.js
@@ -2,11 +2,66 @@ import React, { Component } from 'react';
 import './App.scss';
 import { connect } from 'react-redux';
 import CandidateTable from './candidate-table';
+import moment from 'moment';
 
 class App extends Component {
 
   constructor(props) {
-    super(props)
+    super(props);
+
+    const columnFormatter = (cell, rowData, rowIdx, extraFormatData) => {
+
+      if (extraFormatData.columnType === "testDetails") {
+        if (rowData.testDetails.score < 3) {
+          return "Rejected";
+        } else {
+          return "Selected";
+        }
+      }
+
+      if (extraFormatData.columnType === "l1Details") {
+        switch (extraFormatData.columnField) {
+          case "score":
+            if (rowData.testDetails.score >= 3) {
+              if (rowData.l1Details.score !== null) {
+                return rowData.l1Details.score;
+              } else {
+                return (
+                  <button>Schedule L1</button>
+                );
+              }
+            } else {
+              return "NA";
+            }
+          case "status":
+            if (rowData.l1Details.score !== null) {
+
+              if (rowData.l1Details.score >= 3) {
+
+                var start = new moment(rowData.l1Details.startTime);
+                var end = new moment();
+
+                if (rowData.l1Details.endTime !== null && rowData.l1Details.endTime.length !== 0) {
+                  end = new moment(rowData.l1Details.endTime);
+                }
+                var duration = moment.duration(end.diff(start));
+
+                return (
+                  <span>
+                    Selected {duration.asMinutes()} mins
+                                </span>
+                );
+              } else {
+                return "Rejected";
+              }
+            } else {
+              return "NA";
+            }
+          default: break;
+        }
+
+      }
+    };
 
     this.columns = [{
       dataField: 'name',
@@ -22,30 +77,55 @@ class App extends Component {
       text: 'Test Status',
       editable: (content, row, rowIndex, columnIndex) => {
         return row.testDetails.score > 3;
+      },
+      formatter: columnFormatter,
+      formatExtraData: {
+        "columnType": "testDetails",
+        "columnField": "status"
       }
     }, {
       dataField: 'l1Details.score',
       text: 'L1 Score',
       editable: (content, row, rowIndex, columnIndex) => {
         return row.testDetails.score > 3;
+      },
+      formatter: columnFormatter,
+      formatExtraData: {
+        "columnType": "l1Details",
+        "columnField": "score"
       }
     }, {
       dataField: 'l1Details.startTime',
       text: 'L1 Status',
       editable: (content, row, rowIndex, columnIndex) => {
         return row.l1Details.score !== "NA" && row.l1Details.score > 3;
+      },
+      formatter: columnFormatter,
+      formatExtraData: {
+        "columnType": "l1Details",
+        "columnField": "status"
       }
     }, {
       dataField: 'gkDetails.score',
       text: 'GK Score',
       editable: (content, row, rowIndex, columnIndex) => {
         return row.l1Details.score !== "NA" && row.l1Details.score > 3;
+      },
+      formatter: columnFormatter,
+      formatExtraData: {
+        "columnType": "gkDetails",
+        "columnField": "score"
       }
     }, {
       dataField: 'gkDetails.startTime',
       text: 'GK Status',
       editable: (content, row, rowIndex, columnIndex) => {
         return row.gkDetails.score !== "NA" && row.gkDetails.score > 3;
+      },
+      formatter: columnFormatter,
+      formatExtraData: {
+        "columnType": "gkDetails",
+        "columnField": "status"
       }
     }];
 
@@ -67,7 +147,7 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = function(store) {
+const mapStateToProps = function (store) {
   return { users: store.users }
 }
 
