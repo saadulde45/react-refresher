62435fd5e43fdf57e37471b960966c6164c7bff6
Saad Ulde <saad.ulde@claricetechnologies.com>
2018-03-20 22:43:23 +0530

    1. Moved store to its separate file

--- interview-scheduler/src/index.js
+++ interview-scheduler/src/index.js
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import reducers from './reducers';
+import store from './store/store';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+
 
 
 ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
+    <Provider store={store}>
         <App />
     </Provider>
     , document.getElementById('root'));
