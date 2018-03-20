af3c4a570c7071f79cc55238822e33a9582fd6cf
vishal lohakare <vishal.lohakare@globant.com>
2018-03-20 15:58:04 +0530

    Added validation for gkdetails & updateCellBackground functionality

--- interview-scheduler/src/App.js
+++ interview-scheduler/src/App.js
@@ -21,47 +21,102 @@ class App extends Component {
 
       if (extraFormatData.columnType === "l1Details") {
         switch (extraFormatData.columnField) {
-          case "score":
-            if (rowData.testDetails.score >= 3) {
-              if (rowData.l1Details.score !== null) {
-                return rowData.l1Details.score;
-              } else {
-                return (
-                  <button>Schedule L1</button>
-                );
-              }
-            } else {
-              return "NA";
-            }
-          case "status":
-            if (rowData.l1Details.score !== null) {
-
-              if (rowData.l1Details.score >= 3) {
-
-                var start = new moment(rowData.l1Details.startTime);
-                var end = new moment();
-
-                if (rowData.l1Details.endTime !== null && rowData.l1Details.endTime.length !== 0) {
-                  end = new moment(rowData.l1Details.endTime);
+            case "score":
+                if (rowData.testDetails.score >= 3) {
+                    if (rowData.l1Details.score !== null) {
+                        return rowData.l1Details.score;
+                    } else {
+                        return (
+                            <button>Schedule L1</button>
+                        );
+                    }
+                } else {
+                    return "NA";
                 }
-                var duration = moment.duration(end.diff(start));
+            case "status":
+                if (rowData.testDetails.score >= 3 && rowData.l1Details.score !== null) {
+                    
+                    if (rowData.l1Details.score >= 3) {
 
-                return (
-                  <span>
-                    Selected {duration.asMinutes()} mins
-                  </span>
-                );
-              } else {
-                return "Rejected";
-              }
-            } else {
-              return "NA";
-            }
-          default: break;
+                        var start = new moment(rowData.l1Details.startTime);
+                        var end = new moment();
+
+                        if(rowData.l1Details.endTime !== null && rowData.l1Details.endTime.length !== 0) {
+                            end = new moment(rowData.l1Details.endTime);
+                        }
+                        var duration = moment.duration(end.diff(start));
+
+                        return (
+                            <span>
+                                Selected {duration.asMinutes()} mins
+                            </span>
+                        );
+                    } else {
+                        return "Rejected";
+                    }
+                } else {
+                    return "NA";
+                }
+            default: break;
         }
 
       }
-    };
+
+      if (extraFormatData.columnType === "gkDetails") {
+          switch (extraFormatData.columnField) {
+              case "score":
+                  if (rowData.testDetails.score >= 3 && rowData.l1Details.score >= 3) {
+                          if (rowData.gkDetails.score !== null) {
+                              return rowData.gkDetails.score;
+                          } else {
+                              return (
+                                  <button>Schedule L1</button>
+                              );
+                            }
+                  } else {
+                      return "NA";
+                  }
+              case "status":
+                  if (rowData.testDetails.score >= 3 && rowData.l1Details.score >= 3) {
+                      if (rowData.gkDetails.score !== null) {                            
+                          if (rowData.gkDetails.score >= 3) {
+
+                              var start = new moment(rowData.gkDetails.startTime);
+                              var end = new moment();
+
+                              if(rowData.gkDetails.endTime !== null && rowData.gkDetails.endTime.length !== 0) {
+                                  end = new moment(rowData.gkDetails.endTime);
+                              }
+                              var duration = moment.duration(end.diff(start));
+
+                              return (
+                                  <span>
+                                      Selected {duration.asMinutes()} mins
+                                  </span>
+                              );
+                          } else {
+                              return "Rejected";
+                          }
+                      } else {
+                          return "NA";
+                      }
+                  } else {
+                      return "NA";
+                  }   
+              default: break;
+          }
+
+      }
+  };
+  function updateCellBackground(cellValue) {
+    if (cellValue === null || cellValue < 3) {
+        return '#FA8072'
+    } else if(cellValue >= 3 && cellValue <= 5) {
+        return '#ADFF2F'
+    } else {
+        return '#FF7F50'
+    }
+  }
 
     this.columns = [{
       dataField: 'name',
@@ -73,12 +128,22 @@ class App extends Component {
       editable: false
     }, {
       dataField: 'testDetails.score',
-      text: 'Test Score'
+      text: 'Test Score',
+      style: (content, rowData, rowIndex, colIndex) => {
+        return {
+        backgroundColor: updateCellBackground(content)
+        };
+      }                
     }, {
       dataField: 'testDetails.startTime',
       text: 'Test Status',
+      style: (content, rowData, rowIndex, colIndex) => {
+        return {    
+        backgroundColor: updateCellBackground(rowData.testDetails.score)
+        };
+      },
       editable: (content, row, rowIndex, columnIndex) => {
-        return row.testDetails.score > 3;
+        return row.testDetails.score >= 3;
       },
       formatter: columnFormatter,
       formatExtraData: {
@@ -88,8 +153,14 @@ class App extends Component {
     }, {
       dataField: 'l1Details.score',
       text: 'L1 Score',
-      editable: (content, row, rowIndex, columnIndex) => {
-        return row.testDetails.score > 3;
+      style: (content, rowData, rowIndex, colIndex) => {
+        let score = rowData.testDetails.score < rowData.l1Details.score ? rowData.testDetails.score :rowData.l1Details.score;        
+        return {
+            backgroundColor: updateCellBackground(score)
+        };
+      },
+      editable: (content, rowData, rowIndex, columnIndex) => {
+        return rowData.testDetails.score >= 3;
       },
       formatter: columnFormatter,
       formatExtraData: {
@@ -99,8 +170,15 @@ class App extends Component {
     }, {
       dataField: 'l1Details.startTime',
       text: 'L1 Status',
-      editable: (content, row, rowIndex, columnIndex) => {
-        return row.l1Details.score !== "NA" && row.l1Details.score > 3;
+      style: (content, rowData, rowIndex, colIndex) => {
+        let score = rowData.testDetails.score < rowData.l1Details.score ? rowData.testDetails.score :rowData.l1Details.score;        
+        return {
+            backgroundColor: updateCellBackground(score)
+        };
+      },
+      editable: (content, rowData, rowIndex, columnIndex) => {
+        return rowData.l1Details.score !== "NA" && rowData.testDetails.score >=3
+        && rowData.l1Details.score >= 3;
       },
       formatter: columnFormatter,
       formatExtraData: {
@@ -110,8 +188,17 @@ class App extends Component {
     }, {
       dataField: 'gkDetails.score',
       text: 'GK Score',
-      editable: (content, row, rowIndex, columnIndex) => {
-        return row.l1Details.score !== "NA" && row.l1Details.score > 3;
+      style: (content, rowData, rowIndex, colIndex) => {
+        let score = rowData.testDetails.score < rowData.l1Details.score 
+                    ? (rowData.testDetails.score < rowData.gkDetails.score ? rowData.testDetails.score : rowData.gkDetails.score)
+                    : (rowData.l1Details.score < rowData.gkDetails.score ? rowData.l1Details.score : rowData.gkDetails.score);
+        return {
+            backgroundColor: updateCellBackground(score)
+        };
+      },
+      editable: (content, rowData, rowIndex, columnIndex) => {
+        return rowData.l1Details.score !== "NA" && rowData.testDetails.score >=3
+        && rowData.l1Details.score >= 3;
       },
       formatter: columnFormatter,
       formatExtraData: {
@@ -121,8 +208,17 @@ class App extends Component {
     }, {
       dataField: 'gkDetails.startTime',
       text: 'GK Status',
-      editable: (content, row, rowIndex, columnIndex) => {
-        return row.gkDetails.score !== "NA" && row.gkDetails.score > 3;
+      style: (content, rowData, rowIndex, colIndex) => {
+        let score = rowData.testDetails.score < rowData.l1Details.score 
+                    ? (rowData.testDetails.score < rowData.gkDetails.score ? rowData.testDetails.score : rowData.gkDetails.score)
+                    : (rowData.l1Details.score < rowData.gkDetails.score ? rowData.l1Details.score : rowData.gkDetails.score);
+        return {
+            backgroundColor: updateCellBackground(score)
+        };
+    },
+      editable: (content, rowData, rowIndex, columnIndex) => {
+        return rowData.gkDetails.score !== "NA" && rowData.testDetails.score >=3
+        && rowData.l1Details.score >= 3 && rowData.gkDetails.score >= 3;
       },
       formatter: columnFormatter,
       formatExtraData: {
